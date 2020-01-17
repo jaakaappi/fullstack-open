8c0b259a72b6fa0b8d62599e83fe60a5179ad422
Esa <diapaasi@gmail.com>
2020-01-17 21:08:27 +0200

    Tehtävä 1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,13 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = props => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = props => {
+  return (
+    <>
+      {props.contents.map(content => {
+        return (
+          <p>
+            {content.part} {content.exercises}
+          </p>
+        );
+      })}
+    </>
+  );
+};
+
+const Total = props => {
+  return <p>Number of exercises {props.total}</p>;
+};
 
 const App = () => {
-    console.log('Hello from komponentti')
-    return (
-      <div>
-        <p>Hello world</p>
-      </div>
-    )
-  }
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
-ReactDOM.render(<App />, document.getElementById('root'))
+  const contents = [
+    { part: part1, exercises: exercises1 },
+    { part: part2, exercises: exercises2 },
+    { part: part3, exercises: exercises3 }
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content contents={contents} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
