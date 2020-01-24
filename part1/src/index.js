464f3c94f1df69455ce8c68bd27f154cdca61108
Esa <diapaasi@gmail.com>
2020-01-24 19:07:11 +0200

    1.3

--- part1/src/index.js
+++ part1/src/index.js
@@ -17,7 +17,7 @@ const Content = props => {
   return (
     <>
       {props.parts.map(part => {
-        return <Part partTitle={part.part} exercises={part.exercises} />;
+        return <Part partTitle={part.name} exercises={part.exercises} />;
       })}
     </>
   );
@@ -29,24 +29,24 @@ const Total = props => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
-  const parts = [
-    { part: part1, exercises: exercises1 },
-    { part: part2, exercises: exercises2 },
-    { part: part3, exercises: exercises3 }
-  ];
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={[part1, part2, part3]} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
