0311b287b77d7bd6fba25ce3b78e451c15d39119
Esa <diapaasi@gmail.com>
2020-03-28 18:21:19 +0200

    Tehtävä 1.4

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -16,37 +16,48 @@ const Part = props => {
 const Content = props => {
   return (
     <>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      {props.parts.map(part => {
+        return <Part key={part.name} part={part} />;
+      })}
     </>
   );
 };
 
 const Total = props => {
-  return <p>Number of exercises {props.total}</p>;
+  console.log(props.parts);
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.exercises,
+        0
+      )}
+    </p>
+  );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
