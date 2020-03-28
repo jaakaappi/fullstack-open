998614907e5b66259d87ec6a514a6e3918ab4b67
Esa <diapaasi@gmail.com>
2020-03-28 19:14:08 +0200

    Osa 1d tekstiosuus

--- osa1/testi/src/index.js
+++ osa1/testi/src/index.js
@@ -2,9 +2,37 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = props => {
-  const [counter, setCounter] = useState(0);
-  setTimeout(() => setCounter(counter + 1), 1000);
-  return <div>{counter}</div>;
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  });
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    };
+    setClicks(newClicks);
+  };
+
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    };
+    setClicks(newClicks);
+  };
+
+  return (
+    <div>
+      <div>
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
