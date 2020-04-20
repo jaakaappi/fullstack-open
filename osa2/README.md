2ce94c2228ec898a24474f315af0362e593d786c
Esa <diapaasi@gmail.com>
2020-04-20 10:55:42 +0300

    Tehtävä 2.1

--- osa2/README.md
+++ osa2/README.md
@@ -1,3 +1,5 @@
 # 2a Kokoelmien renderöinti ja moduulit
 
-TODO
+.map():lla ja muilla toistorakenteilla elementtejä luodessa niillä pitää olla uniikki key-property. Ei suoraan listaindeksi, mielummin uniikki ja pysyvä.
+
+`import Component from './components/Component'`
---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = (props) => {
+  return <h1>{props.name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map((part) => {
+        return <Part key={part.name} part={part} />;
+      })}
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.exercises,
+        0
+      )}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    id: 1,
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
