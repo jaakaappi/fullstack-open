5b038c01d5a9c51120b0b2b15c1f43609fb1942f
Esa <diapaasi@gmail.com>
2020-05-19 16:59:27 +0300

    Tehtävä 2.6

--- osa2/README.md
+++ osa2/README.md
@@ -3,3 +3,9 @@
 .map():lla ja muilla toistorakenteilla elementtejä luodessa niillä pitää olla uniikki key-property. Ei suoraan listaindeksi, mielummin uniikki ja pysyvä.
 
 `import Component from './components/Component'`
+
+# 2b Lomakkeet
+
+- Muista onChange controlled componentteihin.
+- Filter-funktio: `notes.filter(note => note.important === true)` palauttaa noten jos tärkeä. Voi käyttää suoraa booleania tietenkin.
+-
---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChanged = (e) => {
+    setNewName(e.currentTarget.value);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    setNewName("");
+    setPersons(persons.concat(personObject));
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChanged} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleAdd}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => {
+        return <p key={`person-${index}`}>{person.name}</p>;
+      })}
+    </div>
+  );
+};
+
+export default App;
