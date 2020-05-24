1ed7a3d322b534454cd5c08eecf022ad413175fa
Esa <diapaasi@gmail.com>
2020-05-24 19:19:50 +0300

    Tehtävä 2.14

--- osa2/maiden_tiedot/src/components/CountryList.js
+++ osa2/maiden_tiedot/src/components/CountryList.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CountryList = ({ countries, showCountry }) => {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    if (countries.length === 1) {
+      const request = new Request(
+        `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_API_KEY}&query=${countries[0].name}`,
+        { method: "GET" }
+      );
+
+      fetch(request)
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+          setWeather(json.current);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [countries]);
+
   if (countries.length === 1) {
     const country = countries[0];
     return (
@@ -21,6 +45,17 @@ const CountryList = ({ countries, showCountry }) => {
           style={{ width: "100px" }}
           alt={`Map of ${country.name}`}
         />
+        <h2>Weather</h2>
+        {weather !== null && (
+          <>
+            <p>Temperature: {`${weather.temperature} degrees Celcius`}</p>
+            <img src={weather.weather_icons[0]} />
+            <p>
+              Wind:{" "}
+              {`${weather.wind_speed} mph, direction: ${weather.wind_dir}`}
+            </p>
+          </>
+        )}
       </div>
     );
   } else if (countries.length > 10) {
